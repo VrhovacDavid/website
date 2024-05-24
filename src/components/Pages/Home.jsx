5ed54ba020e36dc31cf64d5eb0fb05d58e80e536
VrhovacDavid <david.vrhovac@tilltech.ch>
2024-05-24 15:16:26 +0200

    deleteGradient

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -17,42 +17,7 @@ import PricingNew from './PricingNew';
 import { useLocation } from 'react-router-dom';
 
 const Home = () => {
-  useEffect(() => {
-    const gradient = new Gradient();
-    gradient.initGradient("#gradient-canvas");
 
-    async function connect() {
-      this.shaderFiles = {
-        vertex: "varying vec3 v_color;\n\nvoid main() {...}",  // Der volle Shader-Code bleibt gleich
-        noise: "//\n// Description : Array and textureless GLSL 2D/3D/4D simplex\n// ...",
-        blend: "//\n// https://github.com/jamieowen/glsl-blend\n// ...",
-        fragment: "varying vec3 v_color;\n\nvoid main() {...}"
-      };
-
-      this.conf = {
-        presetName: "",
-        wireframe: false,
-        density: [0.06, 0.16],
-        zoom: 1,
-        rotation: 0,
-        playing: true
-      };
-
-      if (document.querySelectorAll("canvas#gradient-canvas").length < 1) {
-        console.log("DID NOT LOAD HERO STRIPE CANVAS");
-      } else {
-        this.minigl = new MiniGl(this.el, null, null, true);
-        requestAnimationFrame(() => {
-          if (this.el) {
-            this.computedCanvasStyle = getComputedStyle(this.el);
-            this.waitForCssVars();
-          }
-        });
-      }
-    }
-
-    connect();
-  }, []);
 
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, {
@@ -82,7 +47,7 @@ const Home = () => {
         <motion.div className="fixed z-10 top-0 left-0 right-0 h-[4px] bg-gradient-to-r from-fuchsia-400 to-sky-400 origin-left" style={{ scaleX }} />
         <Header />
         <HeroSection />
-        <canvas id="gradient-canvas" className="w-full h-40 md:h-80 -skew-y-6"></canvas>
+  
         <FeatureSection1 />
         <LogoClouds />
         <Section1 />
