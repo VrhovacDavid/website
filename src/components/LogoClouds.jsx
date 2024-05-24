d6c47b2e4ed1df8d28b0d015876486b5586a35e2
VrhovacDavid <david.vrhovac@tilltech.ch>
2024-05-24 07:28:01 +0200

    v8.0

--- src/components/LogoClouds.jsx
+++ src/components/LogoClouds.jsx
@@ -34,7 +34,7 @@ export default function LogoClouds() {
             <div className="bg-primary p-6 sm:p-10">
               <img
                 className="max-h-12 w-full object-contain"
-                src="/src/assets/logos/sms.png"
+                src="./src/assets/logos/sms.png"
                                 alt="Laravel"
                 width={158}
                 height={48}
@@ -43,7 +43,7 @@ export default function LogoClouds() {
             <div className="bg-primary p-6 sm:p-10">
               <img
                 className="max-h-12 w-full object-contain"
-                src="/src/assets/logos/meta.svg"
+                src="./src/assets/logos/meta.svg"
                 alt="SavvyCal"
                 width={140}
                 height={32}
@@ -52,7 +52,7 @@ export default function LogoClouds() {
             <div className="bg-primary p-6 sm:p-10">
               <img
                 className="max-h-12 w-full object-contain"
-                src="/src/assets/logos/Google.svg"
+                src="./src/assets/logos/Google.svg"
                 alt="google"
                 width={158}
                 height={48}
