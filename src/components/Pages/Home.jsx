1ea4ae3b7f79cbd008a7f3dd5e67acd8c9e4a918
VrhovacDavid <david.vrhovac@tilltech.ch>
2024-06-09 09:17:58 +0200

    UninistallIntercom

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -16,25 +16,13 @@ import { Gradient } from "whatamesh";
 import PricingNew from './PricingNew';
 import { useLocation } from 'react-router-dom';
 
-import Intercom from '@intercom/messenger-js-sdk';
 
 const Home = () => {
 
-  const user = {
-    id: String,
-    name: String,
-    email: String,
-    createdAt: String// Unix timestamp in seconds
-  };
 
 
-  Intercom({
-    app_id: 'q10t1bz3',
-    user_id: user.id, // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
-    name: user.name, // IMPORTANT: Replace "user.name" with the variable you use to capture the user's name
-    email: user.email, // IMPORTANT: Replace "user.email" with the variable you use to capture the user's email
-    created_at: user.createdAt, // IMPORTANT: Replace "user.createdAt" with the variable you use to capture the user's sign-up date in a Unix timestamp (in seconds) e.g. 1704067200
-  });
+
+ 
 
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, {
