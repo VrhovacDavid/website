7b10a1c73afda467903811fe02e2949167ab11b3
VrhovacDavid <david.vrhovac@tilltech.ch>
2024-06-14 08:45:02 +0200

    backtonormal

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,15 @@
-import React from "react"
+import React, {useRef, useEffect} from "react"
 
-export default function Example() {
-
-
+export default function Example(props) {
 
 
- 
+
+
+      const videoRef = useRef(undefined);
+      useEffect(() => {
+          videoRef.current.defaultMuted = true;
+      })
+
 
 
   return (
@@ -44,25 +48,21 @@ export default function Example() {
           </div>
         </div>
         <div className="mx-auto mt-16 flex lg:max-w-4xl lg:flex-none xl:ml-32">
-          
-
-
-          <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4"
-          dangerouslySetInnerHTML={{
-            __html: `<video className="w-[86rem] lg:h-[32rem] lg:object-cover rounded-md shadow-2xl ring-1 ring-gray-900/10" autoplay loop muted playsinline width={2432} height={1442}>
-      <source src="/HeroVideoLong.webm" type="video/webm" />
-      <source src="/HeroVideoLong.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
-</video>`,
-          }}
-        />
-
-
-            
+          <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4" >
 
 
 
-         
+
+
+            <video controls autoPlay loop  width={2432} height={1442} ref={videoRef} className="w-[86rem] lg:h-[32rem] lg:object-cover rounded-md shadow-2xl ring-1 ring-gray-900/10">
+              <source src="/HeroVideoLong.webm" type="video/webm" />
+              <source src="/HeroVideoLong.mp4" type="video/mp4" />
+
+            </video>
+
+
+
+          </div>
         </div>
       </div>
     </div>
