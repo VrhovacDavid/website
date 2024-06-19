c6637d1faff7199c258ae6acbeed1061ecc5a52f
VrhovacDavid <david.vrhovac@tilltech.ch>
2024-06-19 09:01:59 +0200

    VideoChangeAndDevDep

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -41,31 +41,21 @@ export default function Example() {
           </div>
         </div>
         <div className="mx-auto mt-16 flex lg:max-w-4xl lg:flex-none xl:ml-32">
-          <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4" >
-           
+
+        <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4"  dangerouslySetInnerHTML={{ __html: `
+        <video
+          loop
+          muted
+          autoplay
+          playsinline
+          src="/HeroVideoLong.webm" 
+          class="w-[86rem] lg:h-[32rem] lg:object-cover rounded-md shadow-2xl ring-1 ring-gray-900/10"
+          className=""
+        />,
+      ` }}></div>
 
 
 
-
-      
-           
-        
-            <video
-              
-              src= "/HeroVideoLong.webm" 
-
-
-
-              playsInline
-              width={2432}
-              height={1442}
-              controls
-              autoPlay
-              loop
-              muted
-              className="w-[86rem] lg:h-[32rem] lg:object-cover rounded-md shadow-2xl ring-1 ring-gray-900/10"
-            />
-          </div>
         </div>
       </div>
     </div>
--- src/components/Portfolio/Keypad.jsx
+++ src/components/Portfolio/Keypad.jsx
@@ -113,7 +113,7 @@ export default function Keypad() {
                 <div className="overflow-hidden py-12">
                     <div className="mx-auto lg:max-w-7xl  max-w-4xl px-6 lg:px-8">
                         <div className="lg:max-w-4xl pb-6 ">
-                            
+
                         </div>
                         <div className="mx-auto grid max-w-4xl grid-cols-1 gap-x-8 pt-4 gap-y-4 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 items-center mt-4">
                             <div className="lg:ml-auto lg:pl-4 lg:pt-4">
@@ -123,19 +123,19 @@ export default function Keypad() {
                                     <p className="text-small font-semibold  text-indigo-600">Konzept</p>
                                     <h3 className="mt-2 text-h3 font-bold tracking-tight text-gray-900 ">Recherche</h3>
                                     <p className="mt-2 text-base sm:text-sup text-gray-600">
-                                    Im ersten Schritt meines Projekts zur Erstellung eines 3D-Modells eines Keypads begann ich mit der Konzeption. Zunächst suchte ich auf Google nach Referenzbildern von existierenden Keypads. Diese halfen mir, verschiedene Designs und Funktionen zu verstehen.
+                                        Im ersten Schritt meines Projekts zur Erstellung eines 3D-Modells eines Keypads begann ich mit der Konzeption. Zunächst suchte ich auf Google nach Referenzbildern von existierenden Keypads. Diese halfen mir, verschiedene Designs und Funktionen zu verstehen.
 
 
                                     </p>
                                     <p className="mt-4 text-base sm:text-sup text-gray-600">
-                                    Anschließend erstellte ich eigene Skizzen, wobei ich die Anordnung der Tasten und die Ergonomie des Keypads berücksichtigte. Diese Kombination aus Recherche und Skizzieren diente als Grundlage für die nächste Phase: die Modellierung in Blender. </p>
+                                        Anschließend erstellte ich eigene Skizzen, wobei ich die Anordnung der Tasten und die Ergonomie des Keypads berücksichtigte. Diese Kombination aus Recherche und Skizzieren diente als Grundlage für die nächste Phase: die Modellierung in Blender. </p>
                                     <div className='my-8'>
-                                    <a
-                  href='/#pricing'
-                  className="rounded-md bg-indigo-50 px-3.5 py-2.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100"
-                >
-                  YouTube Video anschauen
-                </a>
+                                        <a
+                                            href='/#pricing'
+                                            className="rounded-md bg-indigo-50 px-3.5 py-2.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100"
+                                        >
+                                            YouTube Video anschauen
+                                        </a>
 
                                     </div>
                                 </div>
@@ -159,26 +159,26 @@ export default function Keypad() {
                 <div className="overflow-hidden pb-12 ">
                     <div className="mx-auto lg:max-w-7xl  max-w-4xl px-6 lg:px-8">
 
-                    <div className="mx-auto grid max-w-4xl grid-cols-1 gap-x-8 gap-y-4 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 items-center mt-4">
-                    <div className="lg:ml-auto lg:pl-4 lg:pt-4">
+                        <div className="mx-auto grid max-w-4xl grid-cols-1 gap-x-8 gap-y-4 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 items-center mt-4">
+                            <div className="lg:ml-auto lg:pl-4 lg:pt-4">
 
                                 <div className="lg:max-w-lg">
 
                                     <p className="text-small font-semibold  text-indigo-600">Blender</p>
                                     <h3 className="mt-2 text-h3 font-bold tracking-tight text-gray-900 leading-10">Erstellung des 3D Modells</h3>
                                     <p className="mt-6 text-base sm:text-sup text-gray-600">
-                                    Im nächsten Schritt begann ich mit der detaillierten Modellierung des Keypads in Blender. Ich formte das Gehäuse, platzierte die Tasten und fügte feine Details hinzu, um das Modell so realistisch wie möglich zu gestalten.
+                                        Im nächsten Schritt begann ich mit der detaillierten Modellierung des Keypads in Blender. Ich formte das Gehäuse, platzierte die Tasten und fügte feine Details hinzu, um das Modell so realistisch wie möglich zu gestalten.
 
 
                                     </p>
                                     <p className="mt-6 text-base sm:text-sup text-gray-600">
-                                    Nach der Modellierung ging ich zum Texturieren über. Ich wählte passende Materialien und Texturen aus, um die Oberflächen authentisch wirken zu lassen.
+                                        Nach der Modellierung ging ich zum Texturieren über. Ich wählte passende Materialien und Texturen aus, um die Oberflächen authentisch wirken zu lassen.
                                     </p>
                                     <p className="mt-6 text-base sm:text-sup text-gray-600">
-Anschließend setzte ich die Beleuchtung, um das Keypad optimal in Szene zu setzen und die verschiedenen Materialien hervorzuheben.
-</p>
-<p className="mt-6 text-base sm:text-sup text-gray-600">
-Zum Schluss renderte ich das Modell, um hochwertige Bilder zu erzeugen, die die Details und das Design des Keypads perfekt darstellen.</p>
+                                        Anschließend setzte ich die Beleuchtung, um das Keypad optimal in Szene zu setzen und die verschiedenen Materialien hervorzuheben.
+                                    </p>
+                                    <p className="mt-6 text-base sm:text-sup text-gray-600">
+                                        Zum Schluss renderte ich das Modell, um hochwertige Bilder zu erzeugen, die die Details und das Design des Keypads perfekt darstellen.</p>
                                     <div className='my-4'>
 
                                     </div>
@@ -200,7 +200,7 @@ Zum Schluss renderte ich das Modell, um hochwertige Bilder zu erzeugen, die die 
                     <div className="lg:max-w-4xl  ">
                         <h2 className="mt-2 text-h3 font-bold tracking-tight text-gray-900 sm:text-4xl">Materialien</h2>
                         <p className="mt-6 text-base sm:text-sup text-gray-700">
-                        Ich habe für mein Projekt hochwertige Texturen verwendet und deren Form und Farbe angepasst, um sie perfekt auf das Design und die Atmosphäre des KeyPads abzustimmen.
+                            Ich habe für mein Projekt hochwertige Texturen verwendet und deren Form und Farbe angepasst, um sie perfekt auf das Design und die Atmosphäre des KeyPads abzustimmen.
                         </p>
                     </div>
                     <div className="mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2">
@@ -211,7 +211,7 @@ Zum Schluss renderte ich das Modell, um hochwertige Bilder zu erzeugen, die die 
                                 <h3 className='text-h5 font-semibold pb-4 text-center'>Brushed Gold</h3>
                                 <blockquote className="text-base sm:text-sup text-gray-900">
                                     <p>
-                                    Für das Keypad habe ich die Textur "Brushed Gold" verwendet, um eine luxuriöse und edle Oberfläche der Knöpfe zu erzeugen. Diese Textur verleiht dem Keypad eine realistische Metalloptik mit feinen, gebürsteten Linien, die das Licht elegant reflektieren und eine hochwertige Anmutung schaffen.
+                                        Für das Keypad habe ich die Textur "Brushed Gold" verwendet, um eine luxuriöse und edle Oberfläche der Knöpfe zu erzeugen. Diese Textur verleiht dem Keypad eine realistische Metalloptik mit feinen, gebürsteten Linien, die das Licht elegant reflektieren und eine hochwertige Anmutung schaffen.
                                     </p>
                                 </blockquote>
 
@@ -224,7 +224,7 @@ Zum Schluss renderte ich das Modell, um hochwertige Bilder zu erzeugen, die die 
                                 <h5 className='text-h5 font-semibold pb-4 text-center'>Bumped Metal</h5>
                                 <blockquote className="text-base sm:text-sup  text-gray-900">
                                     <p>
-                                    Die Textur "Bumped Metal" kam zum Einsatz, um dem Keypad eine robuste und industrielle Ausstrahlung bei den Tasten zu geben. Diese Textur sorgt für eine strukturierte Oberfläche mit erhabenen Bereichen, die dem Modell Tiefe und Charakter verleihen und gleichzeitig die Funktionalität und Haltbarkeit des Designs betonen.
+                                        Die Textur "Bumped Metal" kam zum Einsatz, um dem Keypad eine robuste und industrielle Ausstrahlung bei den Tasten zu geben. Diese Textur sorgt für eine strukturierte Oberfläche mit erhabenen Bereichen, die dem Modell Tiefe und Charakter verleihen und gleichzeitig die Funktionalität und Haltbarkeit des Designs betonen.
                                     </p>
                                 </blockquote>
 
@@ -245,9 +245,9 @@ Zum Schluss renderte ich das Modell, um hochwertige Bilder zu erzeugen, die die 
                                     <p className="text-small font-semibold  text-indigo-600">Rendering</p>
                                     <h3 className="mt-2 text-h3 font-bold tracking-tight text-gray-900 leading-10">Abschluss</h3>
                                     <p className="mt-6 text-base sm:text-sup text-gray-600">
-                                    Zum Abschluss habe ich das Modell in 4K gerendert, um die feinen Details und die hohe Qualität der Texturen perfekt zur Geltung zu bringen. Das Projekt war ein voller Erfolg und das Keypad sieht sowohl realistisch als auch beeindruckend aus.
+                                        Zum Abschluss habe ich das Modell in 4K gerendert, um die feinen Details und die hohe Qualität der Texturen perfekt zur Geltung zu bringen. Das Projekt war ein voller Erfolg und das Keypad sieht sowohl realistisch als auch beeindruckend aus.
                                     </p>
-                                   
+
                                 </div>
                             </div>
                             <div className="flex items-center justify-center lg:items-start lg:justify-end ">
@@ -270,17 +270,19 @@ Zum Schluss renderte ich das Modell, um hochwertige Bilder zu erzeugen, die die 
 
                                 <div className="lg:max-w-lg">
 
-                                    <p className="text-small font-semibold  text-indigo-600">Schwierigkeiten</p>
+                                    <p className="text-small font-semibold  text-indigo-600">Schwierigkeiten
+
+                                    </p>
                                     <h3 className="mt-2 text-h3 font-bold tracking-tight text-gray-900 leading-10">Herausforderungen</h3>
                                     <p className="mt-6 text-base sm:text-sup text-gray-600">
-                                   Eine der Herausforderungen war, dass ich nur ein einziges Referenzbild zur Verfügung hatte. Um dies zu kompensieren, musste ich kreativ werden und viel Zeit in die Recherche ähnlicher Designs investieren, um fehlende Informationen zu ergänzen.
-                                    <p className="mt-6 text-base sm:text-sup text-gray-600">
-Zusätzlich war die Erstellung realistischer Texturen und Materialien eine Herausforderung. Es erforderte mehrere Iterationen und Anpassungen, um die gewünschten Effekte zu erzielen und sicherzustellen, dass die Texturen nahtlos und authentisch wirkten.</p>
+                                        Eine der Herausforderungen war, dass ich nur ein einziges Referenzbild zur Verfügung hatte. Um dies zu kompensieren, musste ich kreativ werden und viel Zeit in die Recherche ähnlicher Designs investieren, um fehlende Informationen zu ergänzen. </p>
+                                        <p className="mt-6 text-base sm:text-sup text-gray-600">
+                                            Zusätzlich war die Erstellung realistischer Texturen und Materialien eine Herausforderung. Es erforderte mehrere Iterationen und Anpassungen, um die gewünschten Effekte zu erzielen und sicherzustellen, dass die Texturen nahtlos und authentisch wirkten.</p>
 
-<p className="mt-6 text-base sm:text-sup text-gray-600">
-Trotz dieser Herausforderungen konnte ich das Projekt erfolgreich abschließen und ein beeindruckendes 3D-Modell des Keypads erstellen.</p>
-                                    </p>
-                                   
+                                        <p className="mt-6 text-base sm:text-sup text-gray-600">
+                                            Trotz dieser Herausforderungen konnte ich das Projekt erfolgreich abschließen und ein beeindruckendes 3D-Modell des Keypads erstellen.</p>
+                                    
+
                                 </div>
                             </div>
                             <div className="flex items-center justify-center lg:items-start lg:justify-end lg:order-first ">
@@ -337,8 +339,8 @@ Trotz dieser Herausforderungen konnte ich das Projekt erfolgreich abschließen u
                         src="https://www.youtube.com/embed/4WiH9pf2ULQ?si=2TzjHgKzRDOgi528"
                         width="100%"
                         title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>
                     </iframe>
                 </div>
 
